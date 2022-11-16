368c9c02430d517446fd3f824972088798070092
Caio Martins do Nascimento <martinscaio29@gmail.com>
2022-11-16 19:33:32 -0300

    StyleSheet

--- reactCLI/App.js
+++ reactCLI/App.js
@@ -5,17 +5,18 @@ import {StyleSheet, Text, View, Button} from 'react-native';
  * LTI update could not be added via codemod */
 
 const App = () => {
-  const [numero, setContador] = useState(0);
+  const [nome, setNome] = useState('estilos');
 
   const ClickMudeEstado = () => {
-    setContador(numero + 1);
+    setNome('programming wiht caio');
   };
 
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>multi. por (5) {numero * 5}</Text>
-      <Text style={styles.text}>você clicou {numero} vezes</Text>
-      <Button title="Mult." onPress={ClickMudeEstado} />
+      <Text style={styles.text}>{nome}</Text>
+      <View style={styles.button}>
+        <Button title="Mult." onPress={ClickMudeEstado} />
+      </View>
     </View>
   );
 };
@@ -24,14 +25,23 @@ const App = () => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: '#000fff',
+    backgroundColor: '#ffff00',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 5,
+    borderColor: 'pink',
+    borderRadius: 10,
   },
   text: {
-    color: '#ffff',
-    fontSize: 20,
+    color: '#000',
+    fontSize: 30,
+    fontWeight: 'bold',
     fontStyle: 'italic',
+    margin: 10,
+    textTransform: 'uppercase',
+  },
+  button: {
+    width: '30%',
     margin: 10,
   },
 });
